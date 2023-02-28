811f2e6ee2b6b390ae55994f4dd95112a9f19988
kshitijpatil508 <kshitijpatil508@gmail.com>
2023-03-01 03:19:04 +0530

    easy

---
+++ A - Watermelon.js
@@ -0,0 +1,33 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((string) => {
+      return string.trim();
+    });
+
+  main();
+});
+
+function readline() {
+  return inputString[currentLine++];
+}
+// Make a Snippet for the code above this and then write your logic in main();
+
+function main() {
+  var num = readline();
+   if (num % 2 === 0 && num > 2)console.log( "YES");
+  else console.log( "NO");
+}
